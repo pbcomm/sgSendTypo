69db4b8222d5a2d373d579b311445d546737f3c7
Paul Bronshteyn <Paul_Bronshteyn@comcast.com>
2014-05-26 13:16:48 -0400

    Initial commit

---
+++ sg.sendTypo.js
@@ -0,0 +1,39 @@
+/*!
+ * @file sg.sendTypo.js
+ * @version 1.0.0
+ * @author Paul.Bronshteyn
+ * @comment Built by a geek loaded on caffeine ...
+ */
+;(function(win, doc) {
+    var
+        getSelection = function() {
+            var selection = '';
+
+            if (win.getSelection) {
+                selection = win.getSelection();
+            } else if (doc.getSelection) {
+                selection = doc.getSelection();
+            } else if (doc.selection) {
+                selection = doc.selection.createRange().text;
+            }
+
+            return selection;
+        },
+
+        sendTypo = function(str) {
+            var event = new CustomEvent('sgSendTypo', { typo: str });
+            doc.body.dispatchEvent(event);
+        },
+
+        onKeyDown = function(e) {
+            if (e.ctrlKey && e.which === 13) {
+                getSelection() && sendTypo(getSelection());
+            }
+        };
+
+    if (doc.addEventListener) {
+        doc.addEventListener('keydown', onKeyDown, false);
+    } else if (doc.attachEvent)  {
+        doc.attachEvent('keydown', onKeyDown);
+    }
+}(window, document));
